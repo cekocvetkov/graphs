cd3c4bbdeea926584f7b0e677e44f3f5f1d97761
Tsvetan Tsvetkov <tsvetan.tsvetkov@dedalus.eu>
2023-02-14 11:12:43 +0100

    created vertex class

--- graphs/src/graph/Vertex.java
+++ graphs/src/graph/Vertex.java
@@ -1,5 +1,52 @@
 package graph;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Vertex
 {
+    private String name;
+    private boolean visited;
+    private List<Vertex> adjecencyList;
+    
+    public Vertex (String name){
+        this.name = name;
+        this.adjecencyList = new ArrayList<>();
+    }
+    
+    public String getName()
+    {
+        return name;
+    }
+    
+    public void setName( String name )
+    {
+        this.name = name;
+    }
+    
+    public boolean isVisited()
+    {
+        return visited;
+    }
+    
+    public void setVisited( boolean visited )
+    {
+        this.visited = visited;
+    }
+    
+    public List<Vertex> getAdjecencyList()
+    {
+        return adjecencyList;
+    }
+    
+    public void addNeighbour( Vertex vertex )
+    {
+        this.adjecencyList.add( vertex );
+    }
+    
+    @Override
+    public String toString()
+    {
+        return this.name;
+    }
 }
